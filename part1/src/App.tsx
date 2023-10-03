72d3191a862a71bae6c8efbe5581283ff6d561f9
samKit88 <samuel.girma.cx9896@gmail.com>
2023-10-03 04:53:07 -0800

    App react component edited

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -1,11 +1,15 @@
 
 const App = () => {
   console.log("Hello from the component")
+  const a = 87
+  const b = 10
+  const now = new Date()
+  console.log(now.toString(), a+b)
 
   return (
     <div>
       <h1>Hello World</h1>
-
+      <p>The sum of {a} and {b} is {a+b}</p>
     </div>
   )
 }
