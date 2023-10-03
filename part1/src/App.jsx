21af87867d4d96f02e66f2fde2e856893f23773f
samKit88 <samuel.girma.cx9896@gmail.com>
2023-10-03 06:10:54 -0800

    JSX

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,9 +2,14 @@
 
 const App = () => {
   
+  const now = new Date()
+  const a = 45
+  const b = 10
+  console.log(now.toString(), a+b)
   return (
     <div>
       <h1>Hello World</h1>
+      <p>The sum of {a} and {b} is {a+b}</p>
     </div>
   )
 }
